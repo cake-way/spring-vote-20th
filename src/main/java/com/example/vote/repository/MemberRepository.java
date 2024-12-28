ca3ea41de0d1e12243bf261b90452626469d1fab
nhi33 <nhi3373@gmail.com>
2024-12-28 20:42:42 +0900

    feat: MemberRepository 구현

---
+++ src/main/java/com/example/vote/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.example.vote.repository;
+
+import com.example.vote.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+    Optional<Member> findByEmail(String email);
+}
